6b4dc20d7685efcaa26347613f87aa91b98f1080
Tomasz Knapik <knapiktomasz1989@gmail.com>
2016-12-11 12:32:45 +0100

    web service

--- src/service/CurrencyRatesDownloader.java
+++ src/service/CurrencyRatesDownloader.java
@@ -1,5 +1,37 @@
 package service;
 
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Currency;
+import model.CurrencyRates;
+
 public class CurrencyRatesDownloader {
+
+	public BigDecimal downloadRate(Currency from, Currency currencyTo) {
+
+		try {
+			URL url = new URL("http://api.fixer.io/latest?base=" + from + "&symbols=" + currencyTo);
+
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Accept", "application/json");
+
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.findAndRegisterModules();
+			CurrencyRates ratesFromJson = objectMapper.readValue(connection.getInputStream(), CurrencyRates.class);
+			// System.out.println(rates);
+			return ratesFromJson.getRates().get(currencyTo); //pobranie waluty docelowej
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		return null;
+
+	}
 
 }
